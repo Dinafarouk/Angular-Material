138f661ea316a76c19d374a2ed6b4f217019d4ce
Dina Farouk <dina.farouk799@gmail.com>
2019-09-09 12:13:37 +0200

    some editing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductsComponent } from './products/products.component';
 import { MyOrdersComponent } from './my-orders/my-orders.component';
-import {AdminOrdersComponent} from './admin/admin-orders/admin-orders.component'
+import {AdminOrdersComponent} from './admin/admin-orders/admin-orders.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { LoginComponent } from './login/login.component';
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import  {NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
@@ -22,7 +22,7 @@ import { LoginComponent } from './login/login.component';
     AdminOrdersComponent,
     ShoppingCartComponent,
     LoginComponent,
-  
+
   ],
   imports: [
     BrowserModule,
